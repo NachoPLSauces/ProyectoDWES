04932b5066c43d87f0a4500cbe33976f7f989c23
Nacho del Prado Losada <ignacio.pralos@educa.jcyl.es>
2020-11-27 08:53:22 +0100

    Commit inicial

---
+++ indexProyectoDWES.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="webroot/css/estilo.css">
+		
+		<title>NPL - Proyecto DAW2</title>
+	</head>
+	
+	<body>
+		<header>
+			<div class="title">
+                            <div class="logo">
+                                <img src="doc/logo.png" alt="logo">
+                            </div>
+                            
+                            <div>
+                                <h2><a href="../index.html">Proyecto DAW2</a></h2>
+                            </div>				
+			</div>
+			
+			<div class="navegador">
+				<nav>
+					<div>
+						<a href="#">DAW</a>
+					</div>
+					
+					<div>
+						<a href="#">DWES</a>
+					</div>
+					
+					<div>
+						<a href="#">DWEC</a>
+					</div>
+					
+					<div>
+						<a href="#">DIW</a>
+					</div>
+				</nav>
+			</div>
+		</header>
+		
+		<main>
+                    <div class="contenido">
+                        <ul>
+                            <li><a href="doc/EstudioTema1.pdf">Tema 1: Desarrollo Web en Entorno Servidor</a></li>
+                            <li><a href="doc/EstudioTema2.pdf">Tema 2: Instalación, configuración y documentación del entorno de desarrollo y del entorno de explotación</a></li>
+                            <li><a href="../proyectoTema3/indexProyectoTema3.php">Tema 3: Características del lenguaje PHP</a></li>
+                            <li><a href="../proyectoTema4/indexProyectoTema4.php">Tema 4: Características del lenguaje PHP</a></li>
+                            <li><a href="../MtoDeDepartamentosTema4/indexMtoDepartamentosTema4.php">Mantenimiento de Departamentos</a></li>
+                            <li><a href="../proyectoTema5/indexProyectoTema5.php">Tema 5: Desarrollo de aplicaciones web utilizando código embebido.</a></li>
+                        </ul>
+                    </div>
+		</main>
+		
+		<footer>
+			<div>
+				<h3>2020-2021 - Nacho del Prado Losada - ignacio.pralos@educa.jcyl.es</h3>
+			</div>
+		</footer>
+	</body>
+</html>
